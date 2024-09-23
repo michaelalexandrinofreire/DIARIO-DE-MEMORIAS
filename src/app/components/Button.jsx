7b1916ed89e37ff48356b66be3979f9822f617f0
Michael <104912294+michaelalexandrinofreire@users.noreply.github.com>
2024-09-23 18:47:20 -0300

    tela inicial

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Button({
+  label = "Clique Aqui",
+  onClick,
+  type = "button",
+  className = "",
+  color = "bg-blue-500"
+}) {
+  return (
+    <button
+      type={type}
+      className={`px-4 py-2 text-fontProjectBlack font-bold rounded-[10px] ${color} ${className} hover:opacity-80`}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+}
