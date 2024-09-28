4096afb0f69f7628d28056d534e76d6b41302d95
Michael <104912294+michaelalexandrinofreire@users.noreply.github.com>
2024-09-28 20:56:29 -0300

    add page new memories

--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -1,7 +1,7 @@
-export default function Input({ placeholder = "preencher", Id, Name, onChange, type = "text" }) {
+export default function Input({ placeholder = "preencher", Id, Name, onChange, type = "text", className }) {
   return (
     <input
-      className="py-1 px-2 rounded-[10px] w-96 bg-bgProject border-[#616161] border"
+      className={`py-1 px-2 rounded-[10px] w-96 bg-bgProject border-[#616161] border ${className}`}
       type={type}
       id={Id}
       name={Name}
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -5,10 +5,14 @@ import Button from ".././components/Button";
 import { useSignOut } from "react-firebase-hooks/auth";
 import { auth } from "../services/firebaseConfig";
 import { useRouter } from "next/navigation";
+import Input from "../components/Input";
+import { useState } from "react";
 
 export default function Home() {
   const [signOut] = useSignOut(auth);
   const router = useRouter();
+
+  const [showMemoryForm, setShowMemoryForm] = useState(false);
 
   const handleSignOut = async () => {
     const success = await signOut();
@@ -18,7 +22,7 @@ export default function Home() {
   };
 
   return (
-    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="relative flex flex-col min-h-screen sm:p-10 font-[family-name:var(--font-geist-sans)]">
       <header className="flex justify-between w-full">
         <a href="">
           <Image src={logo} priority />
@@ -33,9 +37,58 @@ export default function Home() {
           />
         </div>
       </header>
-      <main className="flex justify-between w-full">
-        
+      <main className="flex justify-start w-full flex-col flex-1">
+        <Button
+          label="NOVA MEMÓRIA"
+          color="bg-transparent"
+          className="bg-transparent border border-[#616161] rounded-[10px] py-1 px-2 text-gray-300 font-medium 
+          hover:bg-[#E0BCF1] hover:text-gray-900 transition-colors w-60 mt-14"
+          onClick={() => setShowMemoryForm(true)}
+        />
+        <h1 className="mt-14">MINHAS MEMÓRIAS</h1>
+        <div className="flex justify-between w-full">
+          <div className="">
+            
+          </div>
+        </div>
       </main>
+
+      {showMemoryForm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+          <div className="relative w-[610px] h-96 bg-[#E0BCF1] rounded-xl border-[35px] border-[#E0BCF1] flex flex-col justify-between gap-4 text-gray-950">
+            <Button
+              label="X"
+              className="absolute -right-6 -top-7 px-1.5 py-0 rounded-full bg-transparent text-xl"
+              onClick={() => setShowMemoryForm(false)}
+            />
+            <div className="flex gap-2">
+              <Input
+                placeholder="TÍTULO"
+                className="bg-transparent w-1/2 placeholder-gray-950"
+              />
+              <Input
+                placeholder="00/00/0000"
+                type="date"
+                className="bg-transparent w-1/2"
+              />
+            </div>
+            <textarea
+              name=""
+              id=""
+              className="bg-transparent border border-[#616161] rounded-[10px] py-1 px-2"
+              rows="8"
+              cols="40"
+            ></textarea>
+            <div className="flex justify-center">
+              <Button
+                label="ENVIAR"
+                className="max-w-min items-center justify-center text-sm text-gray-300"
+                color="bg-[#242424]"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
